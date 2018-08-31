00e4247c9eaeb3d8d75b8a3bab673a9b57095159
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2018-08-31 11:46:10 -0400

    Add standard linting rules and fix errors.

--- src/eosjs2-jssig.ts
+++ src/eosjs2-jssig.ts
@@ -1,35 +1,37 @@
 // copyright defined in eosjs2/LICENSE.txt
 
-'use strict';
+"use strict"
 
-const ecc = require('eosjs-ecc');
-import { SignatureProvider, SignatureProviderArgs } from './eosjs2-api';
+import ecc from "eosjs-ecc"
+import { SignatureProvider, SignatureProviderArgs } from "./eosjs2-api"
 
 /** Signs transactions using in-process private keys */
 export default class JsSignatureProvider implements SignatureProvider {
   /** map public to private keys */
-  keys = new Map<string, string>();
+  public keys = new Map<string, string>()
 
   /** public keys */
-  availableKeys = [] as string[];
+  public availableKeys = [] as string[]
 
   /** @param privateKeys private keys to sign with */
   constructor(privateKeys: string[]) {
-    for (let k of privateKeys) {
-      let pub = ecc.PrivateKey.fromString(k).toPublic().toString();
-      this.keys.set(pub, k);
-      this.availableKeys.push(pub);
+    for (const k of privateKeys) {
+      const pub = ecc.PrivateKey.fromString(k).toPublic().toString()
+      this.keys.set(pub, k)
+      this.availableKeys.push(pub)
     }
   }
 
   /** Public keys associated with the private keys that the `SignatureProvider` holds */
-  async getAvailableKeys() {
-    return this.availableKeys;
+  public async getAvailableKeys() {
+    return this.availableKeys
   }
 
   /** Sign a transaction */
-  async sign({ chainId, requiredKeys, serializedTransaction }: SignatureProviderArgs) {
-    let signBuf = Buffer.concat([new Buffer(chainId, 'hex'), new Buffer(serializedTransaction), new Buffer(new Uint8Array(32))]);
-    return requiredKeys.map(pub => ecc.Signature.sign(signBuf, this.keys.get(pub)).toString());
+  public async sign({ chainId, requiredKeys, serializedTransaction }: SignatureProviderArgs) {
+    const signBuf = Buffer.concat(
+      [new Buffer(chainId, "hex"), new Buffer(serializedTransaction), new Buffer(new Uint8Array(32))],
+    )
+    return requiredKeys.map((pub) => ecc.Signature.sign(signBuf, this.keys.get(pub)).toString())
   }
 }
